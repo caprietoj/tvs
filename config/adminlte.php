c687d5a6f2e5002c234e00a4f1b7b806eee5c76c
Cristian <caprietoj@unadvirtual.edu.co>
2025-09-05 11:42:49 -0500

    feat: comentar sección de presupuesto en el menú de administración

--- config/adminlte.php
+++ config/adminlte.php
@@ -754,13 +754,13 @@ return [
                     'can'  => 'previsitas.view',
                     'active' => ['previsitas*'],
                 ],
-                [
-                    'text' => '💰 Presupuesto',
-                    'url'  => 'presupuesto',
-                    'icon' => 'fas fa-calculator',
-                    'can'  => 'admin',
-                    'active' => ['presupuesto*'],
-                ],
+                // [
+                //     'text' => '💰 Presupuesto',
+                //     'url'  => 'presupuesto',
+                //     'icon' => 'fas fa-calculator',
+                //     'can'  => 'admin',
+                //     'active' => ['presupuesto*'],
+                // ],
             ],
         ],
 
